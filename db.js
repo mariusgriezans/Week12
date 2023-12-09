bef8141892dc8fc970053fa3bd43f1f5dcfcfcdf
Marius G <marius.griezans@gmail.com>
2023-12-09 13:38:38 +0200

    week12

---
+++ db.js
@@ -0,0 +1,13 @@
+const dotenv = require('dotenv');
+dotenv.config();
+const { Pool } = require("pg");
+
+const itemsPool = new Pool ({
+    connectionString: process.env.DBConnectionString,
+    ssl: {
+        rejectUnauthorized: false
+    }
+});
+
+
+module.exports = itemsPool;
---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const recipeRouter = require("./routes/recipes.routes");
+const ingredientRouter = require("./routes/ingredients.routes");
+const fullRecipesRouter = require("./routes/fullRecipes.routes");
+const randomRouter = require("./routes/randomRecipe.routes");
+const app = express();
+
+app.use(express.json());
+
+
+app.use("/recipes", recipeRouter);
+app.use("/ingredients", ingredientRouter);
+app.use("/fullRecipes", fullRecipesRouter);
+app.use("/random", randomRouter);
+
+app.listen(3000, () => {
+    console.log("Server is running on port 3000.");
+}); 
